ba3ca678a309573ce9d7ee91704a53b20ccf71e5
Chris Chuang <chrischuang0421@gmail.com>
2019-04-20 11:45:15 +0800

    Updates

---
+++ precache-manifest.8d7070ba64c3beaf96a4e15953ae7009.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "24791f9a1458395f5c8e33bfb17dea91",
+    "url": "/snake-game/static/media/ic-point.24791f9a.svg"
+  },
+  {
+    "revision": "410390fd3f9983ca59a0df73bd9adeba",
+    "url": "/snake-game/static/media/bg-start.410390fd.svg"
+  },
+  {
+    "revision": "d4699606036388f4309e",
+    "url": "/snake-game/static/js/runtime~main.207c8f96.js"
+  },
+  {
+    "revision": "16b75ff285726efbbe4f",
+    "url": "/snake-game/static/js/main.44341542.chunk.js"
+  },
+  {
+    "revision": "321c612882064a16633c",
+    "url": "/snake-game/static/js/2.e72eb82a.chunk.js"
+  },
+  {
+    "revision": "16b75ff285726efbbe4f",
+    "url": "/snake-game/static/css/main.4516b613.chunk.css"
+  },
+  {
+    "revision": "8a3205864da766ffb2b57df39c193d72",
+    "url": "/snake-game/index.html"
+  }
+];
