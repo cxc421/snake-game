ace929de13bd0bb7cddabcb5e6ba4be56278fe62
Chris Chuang <chrischuang0421@gmail.com>
2019-04-20 23:38:54 +0800

    Add adjust sandbox size when page load function

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,12 +1,31 @@
 import styled from 'styled-components';
 
+const width = 1280;
+const height = 800;
+
+function getScale(w, h) {
+  const { clientHeight, clientWidth } = document.body;
+  if (clientHeight >= h && clientWidth >= w) {
+    return 1;
+  }
+
+  let newW = clientWidth;
+  let newH = (h * newW) / w;
+  if (newH > clientHeight) {
+    return clientHeight / h;
+  } else {
+    return clientWidth / w;
+  }
+}
+
 const Container = styled.div`
   position: absolute;
-  width: 1280px;
-  height: 800px;
+  width: ${width}px;
+  height: ${height}px;
   top: 50%;
   left: 50%;
-  transform: translate(-50%, -50%);
+  transform-origin: center center;
+  transform: translate(-50%, -50%) scale(${getScale(width, height)});
   background: gray;
 `;
 
